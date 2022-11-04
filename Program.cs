75fbc208f17532c5288ab9fd8dd44a2651a272e1
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-04 16:08:44 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsertSortExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
